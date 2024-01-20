a64ef676995e7e084107a890217619dc40b015b2
nyeinminhtet <nyeinmg904@gmail.com>
2024-01-21 01:16:47 +0630

    add-create-server

---
+++ components/modals/create-server-modal.tsx
@@ -0,0 +1,134 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import axios from "axios";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { FormValues, formSchema } from "@/lib/validation/server-validation";
+import FileUpload from "../FileUpload";
+import { useRouter } from "next/navigation";
+import { useModalStore } from "@/hooks/use-modal-store";
+
+const CreateServerModal = () => {
+  const router = useRouter();
+  const { isOpen, onClose, type } = useModalStore();
+
+  const isModalOpen = isOpen && type === "createServer";
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+
+  const isloading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: FormValues) => {
+    try {
+      await axios.post("/api/servers", values);
+
+      form.reset();
+      router.refresh();
+      onClose();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and image. You can always
+            change it later.
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-xs uppercase font-bold text-zinc-500 dark:text-secondary/70">
+                        Upload Server Image
+                      </FormLabel>
+                      <FormControl>
+                        <FileUpload
+                          endPoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-xs uppercase font-bold text-zinc-500 dark:text-secondary/70">
+                      Server Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter server name"
+                        disabled={isloading}
+                        className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                        {...field}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button type="submit" variant="primary" disabled={isloading}>
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+export default CreateServerModal;
--- components/navigation/NavigationAction.tsx
+++ components/navigation/NavigationAction.tsx
@@ -1,14 +1,22 @@
 "use client";
 
-import { Plus } from "lucide-react";
 import React from "react";
+
+import { Plus } from "lucide-react";
+
 import { ActionTooltip } from "../ActionTooltip";
+import { useModalStore } from "@/hooks/use-modal-store";
 
 const NavigationAction = () => {
+  const { onOpen } = useModalStore();
+
   return (
     <div>
       <ActionTooltip align="center" side="right" label="Add a server">
-        <button className="group flex items-center">
+        <button
+          className="group flex items-center"
+          onClick={() => onOpen("createServer")}
+        >
           <div
             className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center
          justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500"
