518ff4fb526709d59367883b99a2e953ec32a377
nyeinminhtet <nyeinmg904@gmail.com>
2024-01-23 23:22:40 +0630

    feat:leav-and-delete-server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -7,6 +7,8 @@ import InviteModal from "../modals/invite-modal";
 import EditServerModal from "../modals/edit-server-modal";
 import MemebersModal from "../modals/memebers-modal";
 import CreateChannelModal from "../modals/create-channel-modal";
+import LeaveServerModal from "../modals/leave-server-modal";
+import DeleteServerModal from "../modals/delete-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -26,6 +28,8 @@ export const ModalProvider = () => {
       <EditServerModal />
       <MemebersModal />
       <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
     </>
   );
 };
